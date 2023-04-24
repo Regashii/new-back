143ce82ef8b1f214462c55218f30b08687b71656
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-24 16:17:32 +0800

    db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const storage = require("./container/storage.js");
+const database = require("./container/database.js");
 
 const app = express();
 const port = 9000;
 
 storage(app);
+database();
 
 app.listen(9000, () => {
   console.log(`port running ${port}`);
