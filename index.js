34c80a40b51ea0d853b8afd15f1239f97d8b5a34
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-24 15:59:05 +0800

    a

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const storage = require("./container/storage.js");
 const app = express();
 const port = 9000;
 
-app.use("/", (req, res) => {
-  res.json({ message: "Hello world" });
-});
 storage(app);
 
 app.listen(9000, () => {
