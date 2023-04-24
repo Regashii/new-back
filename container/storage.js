a7075c8d75edbebc30e218ef3021048781842764
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-24 15:59:30 +0800

    a

--- container/storage.js
+++ container/storage.js
@@ -6,7 +6,9 @@ const admin = require("../routes/admin.js");
 module.exports = function (app) {
   app.use(cors());
   app.use(express.json({ extended: false }));
-
+  app.use("/", (req, res) => {
+    res.json({ message: "Hello world" });
+  });
   app.use("/token", token);
   app.use("/admin", admin);
 };
