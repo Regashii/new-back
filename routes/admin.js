40ecb4ef4549e5938b90f6076a15a224d5938426
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-26 13:29:23 +0800

    json

--- routes/admin.js
+++ routes/admin.js
@@ -3,16 +3,16 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.get("/", authenticateToken, (req, res) => {
-  res.json({ status: "ok" });
+  res.json("Congrats");
 });
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.status(401).json("Unauthorized");
+  if (token == null) return res.json("Unauthorized");
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json("Token is not valid!");
+    if (err) return res.json("Token is not valid!");
     req.user = user;
     next();
   });
